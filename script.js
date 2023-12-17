08ce8f7c6b04216f62d0fd90d3c0f1594e861e1a
patel <parthpatel167@gmail.com>
2023-12-17 13:51:23 -0600

    add es2015 code and clear some error

--- script.js
+++ script.js
@@ -4,38 +4,68 @@ function filterOutOdds() {
     return num % 2 === 0;
   });
 }
+// ES2015
+const filterOutOdds1 = (...arguments) =>
+  arguments.filter((num) => num % 2 === 0);
 
+//ES2015
+const findMin = (...arguments) => Math.min(...arguments);
 findMin(1, 4, 12, -3); // -3
 findMin(1, -1); // -1
 findMin(3, 1); // 1
 
+//ES 2015
+const mergeObjects = (obj1, obj2) => ({ ...obj1, ...obj2 });
 mergeObjects({ a: 1, b: 2 }, { c: 3, d: 4 }); // {a:1, b:2, c:3, d:4}
 
+//es2015
+const doubleAndReturnArgs = (arr, ...arguments) => [
+  ...arr,
+  ...arguments.map((num) => num * 2),
+];
 doubleAndReturnArgs([1, 2, 3], 4, 4); // [1,2,3,8,8]
 doubleAndReturnArgs([2], 10, 4); // [2, 20, 8]
 
 /** remove a random element in the items array
 and return a new array without that item. */
 
-function removeRandom(items) {}
+const removeRandom = (items) => {
+  let index = Math.floor(Math.random() * items.length);
+  return [...items.slice(0, index), ...items.slice(index + 1)];
+};
 
 /** Return a new array with every item in array1 and array2. */
 
-function extend(array1, array2) {}
+const extend = (array1, array2) => {
+  return [...array1, ...array2];
+};
 
 /** Return a new object with all the keys and values
 from obj and a new key/value pair */
 
-function addKeyVal(obj, key, val) {}
+const addKeyVal = (obj, key, value) => {
+  let newObj = { ...obj };
+  newObj[key] = value;
+  return newObj;
+};
 
 /** Return a new object with a key removed. */
 
-function removeKey(obj, key) {}
+const removeKey = (obj, key) => {
+  let newObj = { ...obj };
+  delete newObj[key];
+  return newObj;
+};
 
 /** Combine two objects and return a new object. */
-
-function combine(obj1, obj2) {}
+const combine = (obj1, obj2) => {
+  return { ...obj1, ...obj2 };
+};
 
 /** Return a new object with a modified key and value. */
 
-function update(obj, key, val) {}
+const update = (obj, key, value) => {
+  let newObj = { ...obj };
+  newObj[key] = value;
+  return newObj;
+};
